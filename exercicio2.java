6f37fe105eaec2220f8272ba77db256a7dc40858
tomastomvi <133170260+tomastomvi@users.noreply.github.com>
2025-03-28 20:26:25 -0300

    Add files via upload

---
+++ exercicio2.java
@@ -0,0 +1,16 @@
+package postgresql;
+
+public class exercicio2 {
+
+    public static void main(String[]args){
+
+        int numero = 7;
+
+        if(numero >=0){
+        System.out.println( numero + " é positivo");
+        } else{
+            System.out.println( numero + " é negativo ");
+        }
+    }
+
+}

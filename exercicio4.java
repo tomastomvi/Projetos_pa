6be84454003177fe062e1ce7127fe5add35ac448
tomastomvi <133170260+tomastomvi@users.noreply.github.com>
2025-03-28 20:46:37 -0300

    Add files via upload

---
+++ exercicio4.java
@@ -0,0 +1,17 @@
+ package postgresql;
+
+public class exercicio4 {
+    public static void main(String[] args) {
+        
+        int numero = 5;
+
+        if (numero % 2==0) {
+            System.out.println("o numero " + numero + " é par ");
+            
+        } else{
+            System.out.println("o numero " + numero + " é impar ");
+        }
+    }
+
+    
+}

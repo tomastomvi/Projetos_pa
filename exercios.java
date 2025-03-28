97dd4133e5b8cced514363fe001ab4e67a31f44a
tomastomvi <133170260+tomastomvi@users.noreply.github.com>
2025-03-28 20:10:04 -0300

    Add files via upload

---
+++ exercios.java
@@ -0,0 +1,18 @@
+package postgresql;
+
+public class exercios {
+
+    public static void main(String[] args) {
+
+        int numero = 10;
+
+        if (numero % 2==0) {
+            System.out.println(numero + " é um numero par ");
+            
+        } else{
+            System.out.println(numero+ " é  UM NUMERO ímpar ");
+        }
+    
+        
+    }
+}

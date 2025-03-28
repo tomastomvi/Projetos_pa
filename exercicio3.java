71acfdad770e4bcb2041b99a2a4a61f9b0b5463b
tomastomvi <133170260+tomastomvi@users.noreply.github.com>
2025-03-28 20:46:20 -0300

    Add files via upload

---
+++ exercicio3.java
@@ -0,0 +1,17 @@
+package postgresql;
+
+public class exercicio3{
+
+    public static void main(String[] args) {
+     
+        int numero = 150;
+
+        if ( numero > 100 ) {
+            System.out.println("o numero " + numero + " é maior que 100");
+        } else {
+            System.out.println("o nuemro " + numero + " é menor que 100");
+        }
+        
+        
+    }
+}

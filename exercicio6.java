0ef74aef76069d383f58617a7665f487bb4ad2d3
tomastomvi <133170260+tomastomvi@users.noreply.github.com>
2025-03-28 21:51:50 -0300

    Add files via upload

---
+++ exercicio6.java
@@ -0,0 +1,12 @@
+public class exercicio6{
+    public static void main(String[] args) {
+        
+        int numero = 10;
+        int numero2 = 20;
+
+         int soma = numero + numero2 ;
+        
+         System.out.println("A soma entre " + numero + "+" + numero2 + "=" + soma );
+
+    }
+}

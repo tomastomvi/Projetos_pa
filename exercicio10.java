55cb6e3cf87a7990d0c4302ec9cd7d284bbce45e
tomastomvi <133170260+tomastomvi@users.noreply.github.com>
2025-03-28 22:37:09 -0300

    Add files via upload

---
+++ exercicio10.java
@@ -0,0 +1,15 @@
+public class exercicio10{
+    public static void main(String[] args) {
+        
+        double numero = 25;
+        double numero2 = 20;
+        double soma = numero + numero2;
+
+        if (soma>=50) {
+            System.out.println("A soma de " + numero + " + " + numero2 + " é igual a " + soma +" e é maior que 50");
+            
+        } else{
+            System.out.println("A soma de " + numero + " + " + numero2 + " é igual a " + soma +" e é menor que 50");
+        }
+    }
+}

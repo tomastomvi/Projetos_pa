6fc850fab7f0becf1aea8da7b165b313a78c12c7
tomastomvi <133170260+tomastomvi@users.noreply.github.com>
2025-03-28 22:24:52 -0300

    Add files via upload

---
+++ exercicio9.java
@@ -0,0 +1,10 @@
+public class exercicio9{
+
+    public static void main(String[] args) {
+        
+        String nome = "joão";
+        int idade = 25;
+
+        System.out.println( nome + " tem " + idade + " anos ");
+    }
+}

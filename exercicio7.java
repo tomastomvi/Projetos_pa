6ec3eb116537f2853116b21d45c23c1590b7c1f2
tomastomvi <133170260+tomastomvi@users.noreply.github.com>
2025-03-28 22:03:48 -0300

    Add files via upload

---
+++ exercicio7.java
@@ -0,0 +1,10 @@
+public class exercicio7{
+    public static void main(String[] args) {
+        
+        int numero = 5;
+        int numero2 = 4;
+        int multiplicação = numero * numero2;
+
+        System.out.println(" A multiplicão entre " + numero + " e " + numero2 + " é " + multiplicação);
+    }
+}

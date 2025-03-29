3a83efc69f58108b11627e7c3c410b048d20df42
tomastomvi <133170260+tomastomvi@users.noreply.github.com>
2025-03-28 22:17:30 -0300

    Add files via upload

---
+++ exercicio 8.java
@@ -0,0 +1,11 @@
+public class execico8{
+    public static void main(String[] args) {
+        
+        int numero = 5;
+        double numero2 = 2.5;
+        double soma = numero + numero2;
+
+        System.out.println("A soma entre " + numero + " e " + numero2 + " é " + soma);
+    }
+    
+}
